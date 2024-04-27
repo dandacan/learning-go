8005a120d70cbe41e7226c3bd47230356d6ed069
dkallman13 <dkallman13@gmail.com>
2024-04-27 12:04:48 -0400

    hello world updated

--- hello world.go
+++ hello world.go
@@ -2,8 +2,15 @@ package main
 
 import "fmt"
 
+//cant use :=
+var thing string = "some letters have been added"
+
 func main() {
+	//standard hello world using vars that are declared differently
 	var hi string = "hello"
 	world := " world"
+
+	//printing the strings
 	fmt.Println(hi + world)
+	fmt.Println(thing)
 }
